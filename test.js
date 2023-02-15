4a63bde3d0666067dfc57e487b0d9eb34be8cfbd
vaishnavireddy5b3 <123533883+vaishnavireddy5b3@users.noreply.github.com>
2023-02-15 09:37:28 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+let z="abc";
+console.log(z);
+const name="sara";
+console.log(name);
+let greet=x=>console.log(x);
+greet('hello');
+let sum=(d,e)=>
+{
+    let result=d+e;
+    return result;
+}
+let result1=sum(2,3);
+console.log(result1);
